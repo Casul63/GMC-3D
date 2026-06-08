eb46dbc7e664ff6d366ce2fd49048cf1ce0ba69d
Casul63 <ikhsanhafidz09@gmail.com>
2026-06-08 11:53:29 +0700

    update model and changed model camera initial position

--- src/routes/model/+page.svelte
+++ src/routes/model/+page.svelte
@@ -114,7 +114,7 @@
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     _renderer.toneMapping = THREE.ACESFilmicToneMapping;
     _renderer.toneMappingExposure = 0.6;
-    camera.position.set(4, 3, 1);
+    camera.position.set(4, 3, 3);
 
     canvasContainer.appendChild(renderer.domElement);
 
